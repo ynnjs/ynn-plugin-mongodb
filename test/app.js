ec3689cb213f11fff72b4bfebd84188d947e6df6
busyhe <525118368@qq.com>
2019-03-25 17:46:05 +0800

    change

--- test/app.js
+++ test/app.js
@@ -18,12 +18,19 @@ const app = new Ynn( {
     },
     routers() {
         const router = this.router;
-
         router.get( '/', async ( ctx, next, rt ) => {
+            const db = rt.app.db;
+            const mongoose = db.mongoose
+            const Schema = mongoose.Schema
+            const model = db.connection.model('users', new Schema({
+                user_name: String
+            }))
+            await model.create({user_name: 'busyhe'})
+            await model.deleteMany({user_name: 'busyhe'})
             rt.response( {} );
         } );
     }
 } );
 
-module.parent || app.listen( Ynn.cargs.port );
+module.parent || app.listen( 3000 );
 module.exports = app;
